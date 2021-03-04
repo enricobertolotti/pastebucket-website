97af131b692ac931ae18b25298a1c7618cf372ac
enricobertolotti <hello@enricobertolotti.com>
2021-03-04 22:34:48 +0100

    Add sass dependency and initial header content

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="d-flex w-100 justify-content-center px-5">
+  <div class="d-flex w-100 justify-content-center py-4">
     <div
       class="d-flex w-100 justify-content-between align-items-center"
       style="max-width: 900px"
     >
-      <h4 id="logo-text">Pastebucket</h4>
-      <a href="https://github.com/enricobertolotti/pastebucket " target="_blank"
+      <div>
+        <h4 id="logo-text">Pastebucket</h4>
+      </div>
+      <a
+        href="https://github.com/enricobertolotti/pastebucket "
+        target="_blank"
+        class="link"
         >Github</a
       >
     </div>
