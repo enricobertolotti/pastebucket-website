92780a951ed58176a796029ddc111221e7585d38
enricobertolotti <hello@enricobertolotti.com>
2021-03-04 22:24:40 +0100

    📦 NEW: Init

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex w-100 justify-content-center px-5">
+    <div
+      class="d-flex w-100 justify-content-between align-items-center"
+      style="max-width: 900px"
+    >
+      <h4 id="logo-text">Pastebucket</h4>
+      <a href="https://github.com/enricobertolotti/pastebucket " target="_blank"
+        >Github</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
