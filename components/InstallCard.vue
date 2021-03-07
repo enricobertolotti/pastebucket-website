16bb82f86b07dd1ca662ee0e36f44a5f1b8e2ec2
enricobertolotti <hello@enricobertolotti.com>
2021-03-07 20:38:20 +0100

    Initial install page

---
+++ components/InstallCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex flex-column align-items-md-start">
+    <div class="d-flex">
+      <div class="number">
+        <p class="m-0">{{ number }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Prop } from 'vue-property-decorator'
+
+export default class InstallCard extends Vue {
+  @Prop({ required: true }) title!: string
+  @Prop({ required: true }) number!: string
+}
+</script>
+
+<style lang="scss" scoped>
+.number {
+  width: 54px;
+  height: 54px;
+  border-radius: 27px;
+}
+</style>
--- pages/install.vue
+++ pages/install.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="container"><p class="m-0">Waow</p></div>
+  <div
+    class="d-flex flex-column mx-auto"
+    style="padding-top: 120px; max-width: 800px"
+  >
+    <h1 class="header-text mx-4">
+      Just a couple more steps to your very own PasteBucket
+    </h1>
+    <p class="description-text mx-4">
+      You will need a machine that can run docker containers to host the
+      pastebucket server on the network. Pastebucket will only work when this
+      machine is turned on and connected to same network as your devices. For
+      example a Raspberry Pi would be perfect for the task.
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,4 +23,17 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header-text {
+  text-align: center;
+  font-family: 'Lato', sans-serif;
+  font-weight: 700;
+  font-size: 40px;
+}
+
+.description-text {
+  text-align: center;
+
+  opacity: 0.3;
+}
+</style>
